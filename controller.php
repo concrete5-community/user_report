63929bc394c373b4ad9b3f20bb08d8a357d43270
a3020 <a3020@fastmail.com>
2021-02-19 18:28:58 +0100

    Version 1.0

--- controller.php
+++ controller.php
@@ -14,7 +14,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'user_report';
     protected $appVersionRequired = '8.0.0';
-    protected $pkgVersion = '0.9.0';
+    protected $pkgVersion = '1.0';
     protected $pkgAutoloaderRegistries = [
         'src/UserReport' => '\A3020\UserReport',
     ];
